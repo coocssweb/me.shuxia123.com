b62127c353852c8423085ab5fd7439a40070ad0b
coocssweb <1974740999@qq.com>
2019-03-28 18:05:35 +0800

    remove p

--- server/config/mongoDatabase.js
+++ server/config/mongoDatabase.js
@@ -1,3 +1,2 @@
 import Mongoose from 'mongoose';
 Mongoose.connect('mongodb://127.0.0.1:12345/me_shuxia123', { useNewUrlParser: true });
-Mongoose.connect('mongodb://wangjiaxin1991:domybest***@127.0.0.1:58972/shuxia123_com', { useNewUrlParser: true });
