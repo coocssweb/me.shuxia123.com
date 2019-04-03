b1ad3e17ffa04f33e0cd781e38f08d677472410c
coocssweb <1974740999@qq.com>
2019-04-03 19:42:03 +0800

    some

--- multiPages/pages/demo/index.js
+++ multiPages/pages/demo/index.js
@@ -1,2 +1,6 @@
-import 'scss/demo.scss';
-import App from 'appModules/loading';
+// import 'scss/demo.scss';
+// import App from 'appModules/loading';
+
+console.log('1234234132');
+console.log('2323');
+console.log(12341234);
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -65,7 +65,7 @@ class Index extends Component {
                     <div className={className({ 'homeSlogan-content': true })}>
                         <p className={className('homeSlogan-title')}>写漂亮的代码</p>
                         <p className={className('homeSlogan-subtitle')}>我们一直在路上，因为生活就如同一场现场音乐会。</p>
-                        <p className={className('homeSlogan-author')}>-- Porsche · Macan</p>
+                        <p className={className('homeSlogan-author')}>-- Macan</p>
                     </div>
                 </div>
                 <div className={className('homeRecommend')}>
