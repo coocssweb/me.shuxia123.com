db01028746442fdfaeace217bd2403a071e58e4c
coocssweb <1974740999@qq.com>
2019-05-24 16:05:28 +0800

    support for https

--- server/app.js
+++ server/app.js
@@ -22,8 +22,9 @@ app.use(catchError());
 app.use(timeLogger());
 // 静态文件目录，设置后不用设置路由，也可以直接访问
 app.use(serve(`${path.join(__dirname, '..', 'public')}`));
+app.use(serve(`${path.join(__dirname, '..', 'dist')}`));
 // 模板目录
-app.use(templating('dist/', {
+app.use(templating('template/', {
     noCache: Config.noCache,
     watch: Config.watch
 }));
